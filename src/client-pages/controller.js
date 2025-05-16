a64315b3d6c0a498a611fc1cee69257564c64520
Suseendiran P <suseendiran.p@neodove.com>
2025-05-16 20:33:27 +0530

    Page rendering added

--- src/client-pages/controller.js
+++ src/client-pages/controller.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import { getPageHtml } from './service.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello from the client page!');
+router.get('/', async (req, res) => {
+    const htmlText = await getPageHtml('home');
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlText);
+});
+
+router.get('/:pageId', async (req, res) => {
+    const pageId = req.params.pageId;
+    const htmlText = await getPageHtml(pageId);
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlText);
 });
 
 export default router;
--- src/client-pages/service.js
+++ src/client-pages/service.js
@@ -1,3 +1,20 @@
-const getPageHtml = async (pageId) => {
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const myFilePath = path.join(__dirname, 'sample-data.txt');
+
+
+const readHtmlCodeFromSampleTextFile = async () => {
+    const data = await fs.readFile(myFilePath, 'utf8');
+    return data;
 }
+
+const getPageHtml = async (pageId) => {
+    return await readHtmlCodeFromSampleTextFile();
+}
+
+export { getPageHtml };
