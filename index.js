8762d739056f469fdbc5846a1ed6ed9d21c8a549
anandu <anandu.k@neodove.com>
2025-05-16 14:24:54 +0530

    nodemon setup done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -6,8 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use("/", portalRoutes)
-app.set('port', 5001);
+app.set('port', process.env.PORT);
 
 server.listen(app.get('port'), () => {
-    console.log("server started successfully....");
+    console.log(`server started successfully on port ${process.env.PORT}...`);
 })
